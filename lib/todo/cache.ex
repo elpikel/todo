dcc07ea8409e04599f5964fe7e733d8339f74007
Arkadiusz Plichta <el.pikel@gmail.com>
2017-02-09 22:58:34 +0100

    persistance layer

--- lib/todo/cache.ex
+++ lib/todo/cache.ex
@@ -10,24 +10,24 @@ defmodule Todo.Cache do
   end
 
   def init(_) do
+    Todo.Database.start("persist")
     {:ok, Map.new}
   end
 
   def handle_call({:server_process, todo_list_name}, _caller, todo_servers) do
     case Map.fetch(todo_servers, todo_list_name) do
-      {:ok, todo_server} -> 
+      {:ok, todo_server} ->
         {:reply, todo_server, todo_servers}
       :error ->
-        {:ok, new_server} = Todo.Server.start
+        {:ok, new_server} = Todo.Server.start(todo_list_name)
         {:reply, new_server, Map.put(todo_servers, todo_list_name, new_server)}
     end
   end
 
   #   {:ok, cache} = Todo.Cache.start
-  #   Todo.Cache.server_process(cache, "Bob's list")
-  #   Todo.Cache.server_process(cache, "Bob's list")
-  #   Todo.Cache.server_process(cache, "Alice's list")
-  #   bobs_list = Todo.Cache.server_process(cache, "Bob's list")
+  #   Todo.Cache.server_process(cache, "bob_list")
+  #   Todo.Cache.server_process(cache, "alice_list")
+  #   bobs_list = Todo.Cache.server_process(cache, "bob_list")
   #   Todo.Server.add_entry(bobs_list, %{date: {2013, 12, 19}, title: "Dentist"})
   #   Todo.Server.entries(bobs_list, {2013, 12, 19})
   #   Todo.Cache.server_process(cache, "Alice's list") |> Todo.Server.entries({2013, 12, 19})
--- lib/todo/server.ex
+++ lib/todo/server.ex
@@ -2,8 +2,8 @@ defmodule Todo.Server do
   use GenServer
 
   # public interface
-  def start do
-    GenServer.start(Todo.Server, nil)
+  def start(name) do
+    GenServer.start(Todo.Server, name)
   end
 
   def add_entry(pid, entry) do
@@ -15,21 +15,23 @@ defmodule Todo.Server do
   end
 
   # genserver interface implementation
-  def init(_) do
-    {:ok, TodoList.new}
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || TodoList.new}}
   end
 
-  def handle_cast({:add_entry, entry}, todo_list) do
-    {:noreply, TodoList.add_entry(todo_list, entry)}
+  def handle_cast({:add_entry, entry}, {name, todo_list}) do
+    new_state = TodoList.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
   end
 
-  def handle_call({:entries, date}, _, todo_list) do
-    {:reply, TodoList.entries(todo_list, date), todo_list}
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {:reply, TodoList.entries(todo_list, date), {name, todo_list}}
   end
 
-  #{:ok, todo_server} = TodoServer.start
-  #TodoServer.add_entry(todo_server, %{date: {2013, 12, 19}, title: "Dentist"})
-  #TodoServer.add_entry(todo_server, %{date: {2013, 12, 20}, title: "Shopping"})
-  #TodoServer.add_entry(todo_server, %{date: {2013, 12, 19}, title: "Movies"})
-  #TodoServer.entries(todo_server, {2013, 12, 19})
+  # {:ok, todo_server} = Todo.Server.start("Bob_list")
+  # Todo.Server.add_entry(todo_server, %{date: {2013, 12, 19}, title: "Dentist"})
+  # Todo.Server.add_entry(todo_server, %{date: {2013, 12, 20}, title: "Shopping"})
+  # Todo.Server.add_entry(todo_server, %{date: {2013, 12, 19}, title: "Movies"})
+  # Todo.Server.entries(todo_server, {2013, 12, 19})
 end
